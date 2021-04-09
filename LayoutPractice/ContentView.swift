f149859addd7c9f9c598b742edffa0f2f933addc
outstandingboy <outstandingboy@kakao.com>
2021-04-09 14:48:31 +0900

    Initial Commit

---
+++ LayoutPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutPractice
+//
+//  Created by 송인걸 on 2021/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LayoutPractice/LayoutPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutPracticeApp.swift
+//  LayoutPractice
+//
+//  Created by 송인걸 on 2021/04/09.
+//
+
+import SwiftUI
+
+@main
+struct LayoutPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
